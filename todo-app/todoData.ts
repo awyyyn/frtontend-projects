75cbd30efee7faed8a35e16ba45def1cf63c448f
awyyyn <alwin.puche16@gmail.com>
2023-09-08 20:32:30 +0800

    removing db.json

---
+++ todo-app/todoData.ts
@@ -0,0 +1,32 @@
+
+import { todo } from './src/types'
+
+const todoData: todo[] = [
+    {
+      "todo": "Complete a Project",
+      "id": "824d65ca-b573-454e-86d7-d0b1fd2d0b46",
+      "completed": true
+    },
+    {
+      "todo": "Create Todo",
+      "id": "f9971553-833f-47e3-bc0b-f3359df13c21",
+      "completed": false
+    },
+    {
+      "todo": "Exercise at 8:00 AM tomorrow",
+      "id": "a4ac1e06-1775-436f-b392-8802cc5f8f67",
+      "completed": false
+    },
+    {
+      "todo": "Study for next week exam",
+      "id": "8ee00743-0c97-48af-b2f4-612e816a31d0",
+      "completed": true
+    },
+    {
+      "todo": "23",
+      "id": "f1379bfe-dd1d-4f73-a7ed-f4e9668c5cdd",
+      "completed": false
+    }
+  ]
+
+  export default todoData
